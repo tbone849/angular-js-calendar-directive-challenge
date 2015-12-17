27db75ebca632f93ac15b3e5c532fb7fffbb077d
Tim Sarvey <timsarvey@gmail.com>
2015-12-17 13:37:01 -0500

    Add style for current day.

--- app/app.js
+++ app/app.js
@@ -6,11 +6,10 @@ angular.module('calendarDemoApp', [])
 			scope: true,
 			controller: function($scope, $element, $attrs){
 				$scope.range = CalendarRange.getMonthlyRange(new Date());
-				
+				$scope.today = new Date();				
 				$scope.weekdayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+				$scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 				$scope.currentMonth = $scope.range.start.getMonth();
-				console.log($scope.currentMonth);
-				console.log($scope.range.days[0].month);
 			}
 		};
 	});
