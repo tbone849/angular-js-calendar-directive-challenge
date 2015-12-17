bb7126f836f7c5333365b96296a20f45e5432744
Tim Sarvey <timsarvey@gmail.com>
2015-12-17 13:03:15 -0500

    Add calendar html template. Add calendar directive and controller. Style calendar.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,18 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
+	.directive('calendar', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'calendar.html',
+			scope: true,
+			controller: function($scope, $element, $attrs){
+				$scope.range = CalendarRange.getMonthlyRange(new Date());
+				
+				$scope.weekdayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+				$scope.currentMonth = $scope.range.start.getMonth();
+				console.log($scope.currentMonth);
+				console.log($scope.range.days[0].month);
+			}
+		};
+	});
 
 // your controller and directive code go here
